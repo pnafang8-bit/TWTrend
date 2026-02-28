9805f7acbc2415cd97a9adce03b3a02b7ed15c93
pnafang8-bit <pnafang8@gmail.com>
2026-02-28 16:00:32 +0800

    Update TWTrend.py

--- TWTrend.py
+++ TWTrend.py
@@ -12,7 +12,7 @@ def _ipv4_only(host, port, family=0, type=0, proto=0, flags=0):
 return _orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
 socket.getaddrinfo = _ipv4_only
 
-st.set_page_config(layout=“wide”, page_title=“TWTrend Pro RS Dashboard”)
+st.set_page_config(layout="wide", page_title="TWTrend Pro RS Dashboard")
 
 DB_URL = URL.create(
 drivername=“postgresql”,
